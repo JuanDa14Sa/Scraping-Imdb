875b579da1805d44d50d6a260143fbe827ab0d2b
JuanDa14Sa <coziestwharf@gmail.com>
2023-03-02 15:03:22 -0500

    Full scraper

--- scrapper.py
+++ scrapper.py
@@ -1,23 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import re
+import json
 
-html_text = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250').text
+html_text = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250', headers={'Accept-Language': 'en-US,en;q=0.8'}).text
 soup = BeautifulSoup(html_text, 'lxml')
 movies = soup.find_all('td', class_='titleColumn')
-titles = []
-years = []
-rankings = []
+to_json = []
 for index, movie in enumerate(movies):
     title = movie.find('a')
     year = movie.find('span')
-    print(f"Ranking: {index+1}\tTítulo: {title.text}\tYear: {year.text[1:-1]}")
-    rankings.append(index+1)
-    titles.append(title.text)
-    years.append(year.text[1:-1])
+    link = title['href']
 
-titles.insert(0,'Pelicula')
-years.insert(0,'Fecha-Estreno')
-with open('top250movies.csv', 'w', newline='') as file:
-     writer = csv.writer(file)
-     writer.writerows(list(zip(titles,years)))
+    print(f"Ranking: {index+1}\tTítulo: {title.text}\tYear: {year.text[1:-1]}\tLink: https://www.imdb.com{link}")
+    page = requests.get(f"https://www.imdb.com{link}fullcredits/", headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.text, 'html.parser')
+    tmp = soup.find('table', class_='cast_list')
+    info = tmp.find_all('td', class_='primary_photo')
+    actors = []
+    for i in info:
+        actors_name = re.search('title="(.*?)"', str(i)).group(1)
+        actors.append(actors_name)
+    to_json.append({'Ranking': index+1, 'Title': title.text, 'Year': year.text[1:-1], 'Link': f"https://www.imdb.com{link}", 'Actors': actors})
+    
+json.dump(to_json, open('top250movies.json', 'w'), indent=4)
